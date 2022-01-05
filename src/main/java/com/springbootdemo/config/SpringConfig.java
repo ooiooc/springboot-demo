659346835425f2d1ee3a661d156ad51cadd24680
ooiooc <shinjh1102@naver.com>
2022-01-05 15:13:45 +0900

    JPA Entity Manager 추가

--- src/main/java/com/springbootdemo/config/SpringConfig.java
+++ src/main/java/com/springbootdemo/config/SpringConfig.java
@@ -1,13 +1,24 @@
 package com.springbootdemo.config;
 
+import com.springbootdemo.repository.JpaMemberRepository;
 import com.springbootdemo.repository.MemberRepository;
 import com.springbootdemo.repository.MemoryMemberRepository;
 import com.springbootdemo.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.persistence.EntityManager;
+
 @Configuration
 public class SpringConfig {
+
+    private EntityManager em;
+
+    @Autowired
+    public SpringConfig(EntityManager em) {
+        this.em = em;
+    }
 
     @Bean
     public MemberService memberService() {
@@ -16,7 +27,8 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository(){
-        return new MemoryMemberRepository();
+        //return new MemoryMemberRepository();
+        return new JpaMemberRepository(em);
     }
 
 }
