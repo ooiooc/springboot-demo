b5e438b7a6ebe99782ea3351671a266e906f21e6
ooiooc <shinjh1102@naver.com>
2022-01-05 15:15:55 +0900

    @Transactional 추가

--- src/main/java/com/springbootdemo/service/MemberService.java
+++ src/main/java/com/springbootdemo/service/MemberService.java
@@ -5,11 +5,13 @@ import com.springbootdemo.repository.MemberRepository;
 import com.springbootdemo.repository.MemoryMemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
 
 //@Service // 스프링 컨테이너에 서비스 등록
+@Transactional
 public class MemberService {
 
     private final MemberRepository memberRepository;
